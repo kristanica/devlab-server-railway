619af4cf56465391bde2f359b98078b90e3e9c50
LeewellCaputol <LeewellC21@gmail.com>
2025-10-27 14:06:21 +0800

    switching to railway

--- src/admin/admin.ts
+++ src/admin/admin.ts
@@ -1,8 +1,11 @@
 import admin, { ServiceAccount } from "firebase-admin";
-import serviceAccount from "../../serviceAccount.json";
-
+import * as dotenv from "dotenv";
+dotenv.config();
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT as string
+) as ServiceAccount;
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as ServiceAccount),
+  credential: admin.credential.cert(serviceAccount),
   storageBucket: "devlab-b8a1e.firebasestorage.app",
 });
 
